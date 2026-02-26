3a447f82cbe30dc7e4d30596b73e4a52f8726fc3
David Bomba <turbo124@gmail.com>
2026-02-27 08:23:14 +1100

    Self Host normalise company enable_modules field

---
+++ database/migrations/2026_02_26_212104_enable_modules_self_hosted.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\Company;
+use App\Utils\Ninja;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (Ninja::isSelfHost()) {
+            
+            Company::query()->update(['enable_modules' => 0]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
